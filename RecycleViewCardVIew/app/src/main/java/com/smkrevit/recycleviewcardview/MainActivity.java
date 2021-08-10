e4e30761b56aa66719eced4e3b48a2ae446c0ca3
KenNarendraE <arduino.smd@gmail.com>
2021-08-10 15:56:52 +0700

    Tugas Android Studio Minggu 5

--- RecycleViewCardVIew/app/src/main/java/com/smkrevit/recycleviewcardview/MainActivity.java
+++ RecycleViewCardVIew/app/src/main/java/com/smkrevit/recycleviewcardview/MainActivity.java
@@ -1,14 +1,61 @@
 package com.smkrevit.recycleviewcardview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    SiswaAdapter adapter;
+    List<Siswa> siswaList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        load();
+        isiData();
+    }
+
+    public void load(){
+        recyclerView = findViewById(R.id.rcvSiswa);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void isiData(){
+
+        siswaList = new ArrayList<Siswa>();
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("bob", "surabaya"));
+        siswaList.add(new Siswa("steve", "surabaya"));
+        siswaList.add(new Siswa("hanz", "surabaya"));
+        siswaList.add(new Siswa("elatiora", "surabaya"));
+        siswaList.add(new Siswa("hyona", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+        siswaList.add(new Siswa("joni", "surabaya"));
+
+        adapter = new SiswaAdapter(this,siswaList);
+        recyclerView.setAdapter(adapter);
+
+    }
+
+    public void btnTambah(View view) {
+        siswaList.add(new Siswa("JONNI", "DDARJO"));
+        adapter.notifyDataSetChanged();
     }
 }
